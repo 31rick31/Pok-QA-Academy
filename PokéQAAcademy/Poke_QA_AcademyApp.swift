0f6e57d95f371af50f1c78e428b2f92bc5563266
Ricky Memije <76138161+31rick31@users.noreply.github.com>
2025-11-07 09:46:07 -0700

    project init

---
+++ PokéQAAcademy/Poke_QA_AcademyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Poke_QA_AcademyApp.swift
+//  PokéQA Academy
+//
+//  Created by Ricky Memije on 11/7/25.
+//
+
+import SwiftUI
+
+@main
+struct Poke_QA_AcademyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
